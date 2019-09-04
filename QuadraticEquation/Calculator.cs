2519bd8305eb01a48f7495d257623f046bffe3c7
AlexsandrDowgaluk <Dowgaluk88@gmail.com>
2019-09-04 13:28:55 +0300

    Добавьте файлы проекта.

---
+++ QuadraticEquation/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadraticEquation
+{
+    class Calculator
+    {
+
+        public static string DoOperation(double num1, double num2, double num3)
+        {
+
+            string result = "";
+            double a;
+            double x = 0;
+            double x1 = 0;
+            double x2 = 0;
+            a = (num2 * num2) - (4 * num1 * num3);
+            if (a < 0) {
+                result = "Дискриминант равен " + a.ToString() + "\n"+"Квадратное уравнение не имеет корней";
+            }
+            else if (a == 0) {
+
+                x = -num2 / 2 * num1;
+
+                result = "Дискриминант равен " + a.ToString() + "\n" + "Квадратное уравнение имеет один корень" + x.ToString();
+            }
+            else if (a > 0) {
+                x1 = (-num2 + Math.Sqrt(a)) / (2 * num1);
+                x2 = (-num2 - Math.Sqrt(a)) / (2 * num1);
+                result = "Дискриминант равен " + a  + "\n" + "Kвадратное уравнение имеет два корня \n" + "x1 =" + x1 + "\n" + "x2 = " + x2 + "\n" ;
+            }
+
+
+            return result;
+
+
+
+
+
+
+        }
+    }
+
+
+
+}
---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadraticEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            while (!endApp)
+            {
+                string numInput1 = "";
+                string numInput2 = "";
+                string numInput3 = "";
+                string result = "";
+
+                Console.Write("Введите коефициент а :");
+                numInput1 = Console.ReadLine();
+
+
+                double cleanNum1 = 0;
+                int i = Convert.ToInt32(numInput1);
+                while ((!double.TryParse(numInput1, out cleanNum1)) || (i == 0))
+                {
+                    Console.Write("Ошибка ввода или a=0 \n");
+                    Console.Write("Введите коефициент а :");
+                    numInput1 = Console.ReadLine();
+
+                }
+                Console.Write("Введите коефициент b:");
+                numInput2 = Console.ReadLine();
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("Ошибка ввода \n");
+                    Console.Write("Введите коефициент b:");
+                    numInput2 = Console.ReadLine();
+                }
+                Console.Write("Введите коефициент c:");
+                numInput2 = Console.ReadLine();
+                double cleanNum3 = 0;
+                while (!double.TryParse(numInput2, out cleanNum3))
+                {
+                    Console.Write("Ошибка ввода \n");
+                    Console.Write("Введите коефициент с:");
+                    numInput3 = Console.ReadLine();
+                }
+
+                try
+                {
+                    result = Calculator.DoOperation(cleanNum1, cleanNum2, cleanNum3);
+
+                    Console.WriteLine("Результат:" + result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine("------------------------\n");
+
+
+                Console.Write("Нажмите q для закрытия приложения " + "Для продолжения нажмите Enter");
+                if (Console.ReadLine() == "q") endApp = true;
+
+                Console.WriteLine("\n");
+            }
+            return;
+
+
+
+
+
+
+        }
+    }
+}
+
